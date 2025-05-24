30956eb437e5f8bd4609c3b589ea4d6ea302d279
Hemanth-4301 <hemanthachar526@gmail.com>
2025-05-24 09:27:33 +0530

    added few things

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ import pickle
 import numpy as np
 import ast  # For safely evaluating strings as Python literals
 from flask_cors import CORS
+import os
+
 
 app = Flask(__name__)
 CORS(app)
@@ -145,4 +147,5 @@ def predict():
         }), 500
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
